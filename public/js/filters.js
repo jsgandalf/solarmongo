3f7c3877ab5209375833d15fe87be90c6c149e1a
Sean Thomas <sean@atly.com>
2013-12-31 00:00:55 -0700

    leads rough draft

--- public/js/filters.js
+++ public/js/filters.js
@@ -2,10 +2,7 @@
 
 angular.module( 'placeHolder', [] ).filter('placeholder', function(){
   return function(input){
-    //console.log("input==undefined: "+input==undefined);
-    console.log(input);
     if(input===undefined || input==null || input==="undefined"){
-      
       return "N/A";
     } else {
       return input;

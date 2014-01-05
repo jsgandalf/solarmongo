8acdc9f15a3a04f79fd1568610e09edfdbdd6542
Sean Thomas <sean@atly.com>
2014-01-05 15:40:14 -0700

    got the basics of siteSurvey working, need to work on toggle buttons next

--- public/js/controllers/leads.js
+++ public/js/controllers/leads.js
@@ -21,43 +21,42 @@ angular.module('mean.leads').controller('LeadsController', ['$scope', '$routePar
             longitude: null,
             latitude: null,
             notes: this.notes,
-            siteSurvey:[{
-                jobSite: {
-                    firstName:this.firstName,
-                    lastName:this.lastName,
-                    address:this.address,
-                    city:this.city,
-                    state:this.state,
-                    zip:this.zip
-                },
-                siteNotes:{
-                    roofingType:'',
-                    layers:'',
-                    height:'',
-                    pitch:'',
-                    location:'',
-                    eyeAvailability:false,
-                    rafterSpacingAndSize:'',
-                    truss:false,
-                    otherTruss:'',
-                    description:''
-                },
-                pictures: {
-                    site:false,
-                    inverterLocation:false,
-                    powerMeterClosed:false,
-                    powerMeterOpen:false,
-                    shadingProblems:false,
-                    arrayLocation:false,
-                    breakerPanelsClosed:false,
-                    breakerPanelsOpen:false
-                },
-                customerExpectation:{
-                    partBill:'',
-                    fullBill:'',
-                    systemType:''
-                }
-            }]
+            jobSite: {
+                firstName:'',
+                lastName:'',
+                address:'',
+                city:'',
+                state:'',
+                zip:''
+            },
+            siteNotes:{
+                roofingType:'',
+                layers:'',
+                height:'',
+                pitch:'',
+                location:'',
+                eyeAvailability:false,
+                rafterSpacingAndSize:'',
+                truss:false,
+                otherTruss:'',
+                description:''
+            },
+            pictures: {
+                site:false,
+                inverterLocation:false,
+                powerMeterClosed:false,
+                powerMeterOpen:false,
+                shadingProblems:false,
+                arrayLocation:false,
+                breakerPanelsClosed:false,
+                breakerPanelsOpen:false
+            },
+            customerExpectation:{
+                partBill:'',
+                fullBill:'',
+                systemType:''
+            },
+            siteSurveyNotes: ''
         });
         lead.$save(function(response) {
             //$location.path('leads/' + response._id);
@@ -100,13 +99,7 @@ angular.module('mean.leads').controller('LeadsController', ['$scope', '$routePar
 
     $scope.update = function() {
         var lead = $scope.lead;
-        var siteSurvey = $scope.siteSurvey
-        console.log(siteSurvey)
-        /*var siteSurvey = $scope.lead.jobSite;
-        if(siteSurvey){
-            lead.siteSurvey.push(siteSurvey)
-            console.log("here");
-        }*/
+        console.log(lead);
         if (!lead.updated) {
             lead.updated = [];
         }

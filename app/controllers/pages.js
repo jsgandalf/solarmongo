6d6b42b1b97a16596aae6a3e5f6f9a374507ac55
Sean Thomas <sean@atly.com>
2014-01-11 17:59:11 -0700

    changed api docs

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -3,13 +3,23 @@
 exports.contactUs = function(req, res) {
     res.render('pages/contactUs', {
         user: req.user ? JSON.stringify(req.user) : 'null',
-        isLoggedIn: req.user ? true : false
+        isLoggedIn: req.user ? true : false,
+        title: "Contact Us"
     });
 };
 
 exports.aboutUs = function(req, res) {
     res.render('pages/aboutUs', {
         user: req.user ? JSON.stringify(req.user) : 'null',
-        isLoggedIn: req.user ? true : false
+        isLoggedIn: req.user ? true : false,
+        title: "About Us"
+    });
+};
+
+exports.api = function(req, res) {
+    res.render('pages/api', {
+        user: req.user ? JSON.stringify(req.user) : 'null',
+        isLoggedIn: req.user ? true : false,
+        title: "About Us"
     });
 };
--- config/routes.js
+++ config/routes.js
@@ -10,6 +10,7 @@ module.exports = function(app, passport, auth) {
 
     //Setting up the users api
     app.post('/users', users.create);
+
     //forgot password link, requires email
     app.post('/forgot',users.forgotPassword);
     app.get('/reset/:id',users.resetPassword);
@@ -51,29 +52,25 @@ module.exports = function(app, passport, auth) {
     var index = require('../app/controllers/index');
     app.get('/', index.render);
 
-    //Contact us route
+    //Pages route
     var pages = require('../app/controllers/pages');
     app.get('/contactus', pages.contactUs);
     app.get('/aboutus', pages.aboutUs);
+    app.get('/api', pages.api);
 
     //Api auth
     var api = require('../app/controllers/api');
-    app.get('/api', api.docs)
     app.all('/api/signin', passport.authenticate('basic', { session: false }), users.me);
     app.all('/api/signup', api.createUser);
 
     //Api
     app.get('/api/leads', passport.authenticate('bearer', { session: false }), leads.all);
     app.post('/api/leads', passport.authenticate('bearer', { session: false }), leads.create);
-    app.get('/leads/:leadId', passport.authenticate('bearer', { session: false }), leads.show);
-    app.put('/leads/:leadId', passport.authenticate('bearer', { session: false }), auth.lead.hasAuthorization, leads.update);
-    app.del('/leads/:leadId', passport.authenticate('bearer', { session: false }), auth.lead.hasAuthorization, leads.destroy);
+    app.get('/api/leads/:leadId', passport.authenticate('bearer', { session: false }), leads.show);
+    app.put('/api/leads/:leadId', passport.authenticate('bearer', { session: false }), auth.lead.hasAuthorization, leads.update);
+    app.del('/api/leads/:leadId', passport.authenticate('bearer', { session: false }), auth.lead.hasAuthorization, leads.destroy);
 
-    // curl -v http://127.0.0.1:3000/?access_token=123456789
-    /*app.get('/bearer',passport.authenticate('bearer', { session: false }),
-      function(req, res){
-        res.json({ username: req.user.username, email: req.user.email });
-      });*/
+    // curl -v http://localhost:3000/?access_token=123456789
 
-    app.get('/token', passport.authenticate('basic', {session: false}), api.getToken)
+    app.post('/token', passport.authenticate('basic', {session: false}), api.getToken)
 };

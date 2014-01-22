30e03cd8a3db08576e0efff878034259759ec39c
Sean Thomas <seanthomas@Seans-iMac.local>
2014-01-21 21:37:44 -0700

    forward emailing to both address

--- app/controllers/emails.js
+++ app/controllers/emails.js
@@ -6,14 +6,14 @@ var sendgrid  = require('sendgrid')(
 
 exports.sendContactEmail = function(req, res) {
     sendgrid.send({
-      to: 'sean.alan.thomas@gmail.com',
+      to: ['bakwarte@gmail.com', 'sean.alan.thomas@gmail.com'],
       from: 'support@solarmongo.com',
       subject: 'New Inquiry from SolarMongo!',
       html: '<h2>Name: '+req.body.firstName+' '+req.body.lastName+'</h2>'
             +'<h2>Company: '+req.body.companyName+'</h2>'
             +'<h2>Phone: '+req.body.phone+'</h2>'
-            +'<h2>Email: '+req.body.email+'</h2><br/>'
-            +'<p>'+req.body.inquiry+'</p>'
+            +'<h2>Email: '+req.body.email+'</h2><h2>Inquiry:</h2>'
+            +'<h2>'+req.body.inquiry+'</h2>'
     }, function(err, json) {
         if (err) { return console.error(err); }
         res.render('pages/thankyou', {

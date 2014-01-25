c238a49de21b38df7d94d1ae9e6ddb498313273b
Sean Thomas <seanthomas@Seans-iMac.local>
2014-01-25 00:35:43 -0700

    taking out n/a

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module( 'placeHolder', [] ).filter('placeholder', function(){
-  return function(input){
+angular.module( 'placeHolder', [] ).filter('commaFilter', function(){
+  return function(city,state){
+  	console.log(city);
+  	console.log(state);
     if(input===undefined || input==null || input==="undefined"){
       return "N/A";
     } else {

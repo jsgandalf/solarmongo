1115693e86534343abe8cef598bd5c0dd82a4bde
“Sean <sean.alan.thomas@gmail.com>
2014-02-04 22:21:32 -0700

    yo

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//var api = require('../../app/controllers/api');
+
 /**
  * Generic require login routing middleware
  */
@@ -8,35 +10,9 @@ exports.requiresLogin = function(req, res, next) {
     /*if (!req.isAuthenticated()) {
         return res.send(401, 'User is not authorized');
     }*/
+    
     //token can be from the web app - req.user or from the api - req.body.user
-        var token = "";
-        console.log("here"); 
-        if(typeof req.user.token.token)
-            token = req.user.token.token;
-        else if(req.body.access_token)
-            token = req.body.access_token;
-        else
-            return res.send(401, 'You are not authorized');
-        var decoded;
-        try{
-            decoded = User.decode(token);
-        }catch(err){
-            console.log("This is a stack trace error: "+err);
-            return next(err);
-        }
-        //Now do a lookup on that email in mongodb ... if exists it's a real user
-        if (decoded && decoded.email) {
-            User.findUser(decoded.email, token, function(err, user) {
-                if (err) { return next(err); }
-                if (!user) { return next(err); }
-                req.user = user;
-                console.log(user);
-                console.log(req.user);
-                return next(user);
-            });
-        } else {
-            return next("Not authorized");
-        }
+    //api.authenticateToken(req, res, next);
 };
 
 /**
@@ -57,8 +33,6 @@ exports.user = {
  */
 exports.lead = {
     hasAuthorization: function(req, res, next) {
-        console.log(req.lead.account)
-        console.log(req.user.account);
         if (req.lead.account.toString() != req.user.account.toString()) {
             return res.send(401, 'You are not authorized');
         }

341232aaf0fca7d52de3f5e1008582216f1dd8d0
Corri Thomas <corrithomas@Corris-MacBook-Air.local>
2014-03-23 23:56:25 -0600

    changes to pricin page

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
     db: "mongodb://jm9102488aftu:A8$`EN}}2vSVnhjrs#S`@dharma.mongohq.com:10061/app20720888",
     app: {
-        name: "SolarMongo - A Modern CRM - Production"
+        name: "SolarMongo - A Modern CRM"
     },
     stripe: {
         key: "sk_live_RqqMUUv9rPsd3sPkdrv8PAje"

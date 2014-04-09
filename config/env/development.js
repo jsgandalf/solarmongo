575f070b1875f7d07094ac98d81c67160be7e910
“Sean <sean.alan.thomas@gmail.com>
2014-04-08 23:38:49 -0600

    added new data

--- config/env/development.js
+++ config/env/development.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-    db: "mongodb://localhost/mean-dev",
+    db: "mongodb://useradmin:​a13Ba56s7Bjskd93jfkd183JIELKDXCM23498fM@oceanic.mongohq.com:10038/app23922529",
     app: {
         name: "SolarMongo - A Modern CRM - Development"
     },
     stripe: {
         key: "sk_test_tthcngkRTRH1BWnT9HrqhC1u"
     },
-    host: "localhost:3000"
+    host: "solarmongodev.herokuapp.com"
 }

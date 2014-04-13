7ef3ed6e3cb30bafa1e7be5364577fe0a1bba7ad
“Sean <sean.alan.thomas@gmail.com>
2014-04-12 23:59:23 -0600

    upload

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -25,26 +25,32 @@ var mongoose = require('mongoose'),
 exports.addMobilePhoto = function(req, res){
     var file = req.files.photo;
     console.log(file);
-    client.putFile(file.path,"/test/"+file.name, {'x-amz-acl': 'public-read','Content-Length': file.size, 'Content-Type': file.type}, function(err, resp){
-        if(err){
-            console.log(err);
-            res.jsonp({message:err});
-        }else{
-            /*var photo = new Photo({
-                path: req.user.account+"/"+req.params.lead+"/"+files.file.name,
-                name: files.file.name,
-                type: files.file.type,
-                lead: req.params.lead,
-                account: req.user.account,
-                photoType: "lead"
-            });
-            photo.save(function(err){
-                if(err) res.jsonp({"errors": err.errors});
-                res.jsonp(photo);
-            });*/
-            res.jsonp({success:true});
-        }
-    });
+    if (!file || file.size == '0') {
+        res.jsonp({message:"Photo must not be empty."});
+    }else if( file.type == 'image/png' || file.type == 'image/jpeg' || file.type == 'image/gif' ){
+        client.putFile(file.path,"/test/"+file.name, {'x-amz-acl': 'public-read','Content-Length': file.size, 'Content-Type': file.type}, function(err, resp){
+            if(err){
+                console.log(err);
+                res.jsonp({message:err});
+            }else{
+                /*var photo = new Photo({
+                    path: req.user.account+"/"+req.params.lead+"/"+files.file.name,
+                    name: files.file.name,
+                    type: files.file.type,
+                    lead: req.params.lead,
+                    account: req.user.account,
+                    photoType: "lead"
+                });
+                photo.save(function(err){
+                    if(err) res.jsonp({"errors": err.errors});
+                    res.jsonp(photo);
+                });*/
+                res.jsonp({success:true});
+            }
+        });
+    }else{
+        res.jsonp({message:"Not a valid type! Must be jpg, jpeg, gif, or png"});
+    }
 }
 
 exports.add = function(req, res) {

fe311e2a045a82910c5ba515f38b382ec45757b8
“Sean <sean.alan.thomas@gmail.com>
2014-04-12 23:48:53 -0600

    l

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -24,15 +24,16 @@ var mongoose = require('mongoose'),
 
 exports.addMobilePhoto = function(req, res){
     var file = req.files.photo;
-    if (!file || file.size <= '0') {
-        res.jsonp({message:"Photo must not be empty."});
-    }else if( file.type == 'image/png' || file.type == 'image/jpeg' || file.type == 'image/gif' ){
-        client.putFile(file.path,file.path,req.user.account+"/"+req.params.lead+"/"+file.name, {'x-amz-acl': 'public-read','Content-Length': file.size, 'Content-Type': file.type}, function(err, resp){
+    //if (!file || file.size <= '0') {
+    //    res.jsonp({message:"Photo must not be empty."});
+    //}else if( file.type == 'image/png' || file.type == 'image/jpeg' || file.type == 'image/gif' ){
+        //req.user.account+"/"+req.params.lead+"/"+file.name
+        client.putFile(file.path,file.path,"/test", {'x-amz-acl': 'public-read','Content-Length': file.size, 'Content-Type': file.type}, function(err, resp){
             if(err){
                 console.log(err);
                 res.jsonp({message:err});
             }else{
-                var photo = new Photo({
+                /*var photo = new Photo({
                     path: req.user.account+"/"+req.params.lead+"/"+file.name,
                     name: file.name,
                     type: file.type,
@@ -43,12 +44,13 @@ exports.addMobilePhoto = function(req, res){
                 photo.save(function(err){
                     if(err) res.jsonp({"errors": err.errors});
                     res.jsonp(photo);
-                });
+                });*/
+                res.jsonp({success:true});
             }
         });
-    }else{
+    /*}else{
         res.jsonp({message:"Not a valid type! Must be jpg, jpeg, gif, or png"});
-    }
+    }*/
 }
 
 exports.add = function(req, res) {
